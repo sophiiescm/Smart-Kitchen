6419c0f5c265de4621b418bce37bb60e798262a0
06JabG <pk2y4y7mdy@privaterelay.appleid.com>
2026-05-21 10:14:14 +0200

    Update models.py
    Zeile 20

--- backend/models.py
+++ backend/models.py
@@ -17,7 +17,7 @@ class User(Base):
     __tablename__ = "users"
     
     id = Column(BigInteger, primary_key=True, index=True)
-    username = Column(String(255), nullable=False)
+    username = Column(String(255),unique=True, index=True, nullable=False)
     email = Column(String(255), unique=True, index=True, nullable=False)
     password_hash = Column(String(255), nullable=False)
 
